97d67675a880dd799f7aa11e37eb215a49e9e466
laraibhussain1109 <134272774+laraibhussain1109@users.noreply.github.com>
2025-05-09 00:12:25 +0530

    Delete ipl_prediction directory

--- ipl_prediction/apps/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, ProfileDetailView
-
-urlpatterns = [
-    path('signup/', UserCreateView.as_view(), name='user-signup'),
-    path('profile/', ProfileDetailView.as_view(), name='user-profile'),
-]
